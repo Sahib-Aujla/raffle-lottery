a40ef5ec66bcf7e36754841b44da5e2ebdb25a5c
Sahib-Aujla <sahibpreet568@gmail.com>
2025-08-30 13:48:22 +0530

    added Raffle.sol

---
+++ src/Raffle.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public {}
+
+    function pickWinner() public {}
+
+    //Getter Function
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
