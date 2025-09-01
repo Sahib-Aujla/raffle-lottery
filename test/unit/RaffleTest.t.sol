997c34b3ad468f92d8ce5af4482326e063815851
Sahib-Aujla <sahibpreet568@gmail.com>
2025-09-01 16:33:59 +0530

    added test

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -11,8 +11,33 @@ contract RaffleTest is Test {
     Raffle raffle;
     HelperConfig helperConfig;
 
+    uint256 subscriptionId;
+    bytes32 gasLane;
+    uint256 automationUpdateInterval;
+    uint256 raffleEntranceFee;
+    uint32 callbackGasLimit;
+    address vrfCoordinatorV2_5;
+
+    address public PLAYER = makeAddr("player");
+    uint256 public constant STARTING_USER_BALANCE = 10 ether;
+    uint256 public constant LINK_BALANCE = 100 ether;
+
     function setUp() external {
         DeployRaffle deploy = new DeployRaffle();
         (raffle, helperConfig) = deploy.deployRaffle();
+
+        vm.deal(PLAYER, STARTING_USER_BALANCE);
+
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+        subscriptionId = config.subscriptionId;
+        gasLane = config.gasLane;
+        automationUpdateInterval = config.automationUpdateInterval;
+        raffleEntranceFee = config.raffleEntranceFee;
+        callbackGasLimit = config.callbackGasLimit;
+        vrfCoordinatorV2_5 = config.vrfCoordinatorV2_5;
+    }
+
+    function testRaffleStateIsOpen() external view {
+        assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
     }
 }
