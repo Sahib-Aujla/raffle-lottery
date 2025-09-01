e84183e3c3051005fe3481df793ec0c0bbda4519
Sahib-Aujla <sahibpreet568@gmail.com>
2025-09-01 16:03:08 +0530

    added deploy script

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -7,5 +7,21 @@ import {Raffle} from "../src/Raffle.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployRaffle is Script {
-    function run() external returns (Raffle) {}
+    function run() external returns (Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            config.subscriptionId,
+            config.gasLane,
+            config.automationUpdateInterval,
+            config.raffleEntranceFee,
+            config.callbackGasLimit,
+            config.vrfCoordinatorV2_5
+        );
+        vm.stopBroadcast();
+
+        return (raffle, helperConfig);
+    }
 }
